6c29246154e29893eaab981ce5f95adea1945306
felix0821 <felixmontalico@gmail.com>
2022-07-04 21:54:32 -0500

    Felix - Primer commit

---
+++ src/main/java/com/system/sit/ProjectSitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.system.sit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/sit/ProjectSitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.sit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
